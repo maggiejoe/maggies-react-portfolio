c25e5f3514338ad804f3b0afeb691b00fe45bb17
maggiejoe <maggiejoseph94@gmail.com>
2022-07-10 22:17:19 -0400

    resume component functions working, requires styling

---
+++ src/components/Resume/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Resume() {
+    return (
+        <div>
+            <a href='https://drive.google.com/file/d/1NNAUDpiuuY8DYmOMFoztzKa6CqK7dLiP/view?usp=sharing' rel="noreferrer" target="_blank">
+                Click here to view my Resume
+            </a>
+        </div>
+    )
+}
+
+export default Resume
