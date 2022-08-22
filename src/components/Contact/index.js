6a9d03cf6d973fc2259c419f98bfdfd081825403
maggiejoe <maggiejoseph94@gmail.com>
2022-08-22 11:15:04 -0400

    fixed contact form to send emails through emailjs

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -5,39 +5,44 @@ import emailjs from '@emailjs/browser';
 function ContactForm() {
   const [formState, setFormState] = useState({ name: '', email: '', message: '' });
 
-  const [errorMessage, setErrorMessage] = useState('');
+  const [formMessage, setFormMessage] = useState('');
   const { name, email, message } = formState;
+
+  // const form = useRef();
 
   // input validations
   const handleChange = (e) => {
     if (e.target.name === 'email') {
       const isValid = validateEmail(e.target.value);
       if (!isValid) {
-        setErrorMessage('Your email is invalid. Please enter a valid email');
+        setFormMessage('Your email is invalid. Please enter a valid email');
       } else {
-        setErrorMessage('');
+        setFormMessage('');
       }
     } else {
       if (!e.target.value.length) {
-        setErrorMessage(`A ${e.target.name} is required`);
+        const name = e.target.name;
+        setFormMessage(`${name.charAt(0).toUpperCase() + name.slice(1)} is required.`);
       } else {
-        setErrorMessage('');
+        setFormMessage('');
       }
     }
-    if (!errorMessage) {
+    if (!formMessage) {
       setFormState({ ...formState, [e.target.name]: e.target.value });
-      console.log('Handle Form', formState);
+      // console.log('Handle Form', formState);
     }
   };
 
   function sendEmail(e) {
     e.preventDefault();
 
-    emailjs.sendForm('service_0hj3rgb', 'template_39hmh03', "#contactForm", 'iskQ2EzJDVVCqiAV2')
+    emailjs.sendForm('service_0hj3rgb', 'template_ygr0lgr', "#contactForm", 'iskQ2EzJDVVCqiAV2')
       .then((result) => {
         console.log(result.text);
+        console.log("success");
       }, (error) => {
         console.log(error.text);
+        console.log("fail");
       });
   }
 
@@ -46,12 +51,12 @@ function ContactForm() {
       <h2 className='titles text-color pb-3 mb-5 border-bottom border-dark'>Contact Me</h2>
 
       {/* contact form */}
-      <form id="contact-form mb-5" onSubmit={sendEmail}>
+      <form id="contactForm" onSubmit={sendEmail}>
         <div className='d-flex justify-content-center' style={{ height: "80vh" }}>
           <div className='col-6'>
             <div className='pb-3'>
               {/* <label id='text-color' className='form-label' htmlFor="name">Name </label> */}
-              <input type="text" name="name" className='form-control' placeholder="Name" defaultValue={name} onBlur={handleChange} />
+              <input type="text" id="from_name" name="name" className='form-control' placeholder="Name" defaultValue={name} onBlur={handleChange} />
             </div>
             <div className='pb-3'>
               {/* <label id='text-color' className='form-label' htmlFor="email">Email Address </label> */}
@@ -61,12 +66,12 @@ function ContactForm() {
               {/* <label id='text-color' className='form-label' htmlFor="message">Please write a brief message below</label> */}
               <textarea name="message" rows="8" className='form-control' placeholder="Message" defaultValue={message} onBlur={handleChange} />
             </div>
-            {errorMessage && (
+            {formMessage && (
               <div>
-                <p className="error-text">{errorMessage}</p>
+                <p className="error-text">{formMessage}</p>
               </div>
             )}
-            <button className='all-text contact-btn py-1 px-4 rounded-3 border border-dark shadow-lg' type="submit">Submit</button>
+            <button className='all-text contact-btn py-1 px-4 rounded-3 border border-dark shadow-lg' type="submit" value="Send">Submit</button>
           </div>
         </div>
       </form>
