b3c4788454fa15f3744eb8d821960c357271ee70
maggiejoe <maggiejoseph94@gmail.com>
2022-09-15 15:08:26 -0400

    all project overviews and technologies used added to cards and styled

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -108,15 +108,15 @@ function Projects() {
                                 {isHoveringP1 &&
                                     <Col className='hover-text text-center align-items-center'>
                                         <div>
-                                            <h1 className='fs-3 fw-bolder'>Basic Portfolio</h1>
+                                            <h1 className='project-title fs-3 fw-bolder border-bottom border-dark p-2'>Basic Portfolio</h1>
                                         </div>
                                         <div>
-                                            <h2 className='fs-5 fw-bolder'>Project Overview</h2>
-                                            <p className='fs-6 fst-italic'>This was one of the first applications built with the University of Toronto. I challenged my abilities with the technologies below to show you a little bit of who I am and what I've done.</p>
+                                            <h2 className='sub-header fs-5 fw-bolder'>Project Overview</h2>
+                                            <p className='project-text fs-6 fst-italic'>This was one of the first applications built with the University of Toronto. I challenged my abilities with the technologies below to show you a little bit of who I am and what I've done.</p>
                                         </div>
                                         <div>
-                                            <h2 className='fs-5 fw-bolder'>Technologies Used</h2>
-                                            <p className='fs-6 fst-italic'>HTML, CSS</p>
+                                            <h2 className='sub-header fs-5 fw-bolder'>Technologies Used</h2>
+                                            <p className='project-text fs-6 fst-italic'>HTML, CSS</p>
                                         </div>
                                         <a target='_blank' rel='noreferrer' href='https://github.com/maggiejoe/Portfolio.git'>
                                             <span className='bi bi-github work-icon'></span>
@@ -135,19 +135,19 @@ function Projects() {
                         <div className="card border-dark border-2 shadow-lg rounded-3">
                             <img className='shadow-lg' src={project2} alt='Project 2'>
                             </img>
-                            <div className='project-card card-img-overlay d-flex justify-content-start align-items-start px-1 py-0'>
+                            <div className='project-card card-img-overlay d-flex justify-content-start align-items-start'>
                                 {isHoveringP2 &&
                                     <Col className='hover-text text-center align-items-center'>
                                         <div>
-                                            <h1 className='fs-3 fw-bolder'>Code Quiz</h1>
+                                            <h1 className='project-title fs-3 fw-bolder border-bottom border-dark p-2'>Code Quiz</h1>
                                         </div>
                                         <div>
-                                            <h2 className='fs-5 fw-bolder'>Project Overview</h2>
-                                            <p className='fs-6 fst-italic'>This was a quiz I built to test my ability to combine all of the technologies below.</p>
+                                            <h2 className='sub-header fs-5 fw-bolder'>Project Overview</h2>
+                                            <p className='project-text fs-6 fst-italic'>This was a quiz I built to test my ability to combine all of the technologies listed below. It is a timed interactive multiple choice quiz, testing your coding knowledge. You also have the ability to save your high scores and keep track of them in the high score section!</p>
                                         </div>
                                         <div>
-                                            <h2 className='fs-5 fw-bolder'>Technologies Used</h2>
-                                            <p className='fs-6 fst-italic'>HTML, CSS, JavaScript, LocalStorage</p>
+                                            <h2 className='sub-header fs-5 fw-bolder'>Technologies Used</h2>
+                                            <p className='project-text fs-6 fst-italic'>HTML, CSS, JavaScript, LocalStorage</p>
                                         </div>
                                         <a target='_blank' rel='noreferrer' href='https://github.com/maggiejoe/code-quiz.git'>
                                             <span className='bi bi-github work-icon'></span>
@@ -165,19 +165,19 @@ function Projects() {
                         <div className="card border-dark border-2 shadow-lg rounded-3">
                             <img className='shadow-lg' src={project3} alt='Project 3'>
                             </img>
-                            <div className='project-card card-img-overlay d-flex justify-content-start align-items-start px-1 py-0'>
+                            <div className='project-card card-img-overlay d-flex justify-content-start align-items-start'>
                                 {isHoveringP3 &&
                                     <Col className='hover-text text-center align-items-center'>
                                         <div>
-                                            <h1 className='fs-3 fw-bolder'>Basic Portfolio</h1>
+                                            <h1 className='project-title fs-3 fw-bolder border-bottom border-dark p-2'>Trip It</h1>
                                         </div>
                                         <div>
-                                            <h2 className='fs-5 fw-bolder'>Project Overview</h2>
-                                            <p className='fs-6 fst-italic'>Lorem Ipsum</p>
+                                            <h2 className='sub-header fs-5 fw-bolder'>Project Overview</h2>
+                                            <p className='project-text fs-6 fst-italic'>Trip is was the first group project I participated in with the University of Toronto bootcamp. I worked alongside three other developers to build an interactive frontend application. It contains three server-side API's and a polished UI with responsiveness for all devices.</p>
                                         </div>
                                         <div>
-                                            <h2 className='fs-5 fw-bolder'>Technologies Used</h2>
-                                            <p className='fs-6 fst-italic'>Lorem Ipsum</p>
+                                            <h2 className='sub-header fs-5 fw-bolder'>Technologies Used</h2>
+                                            <p className='project-text fs-6 fst-italic'>HTML, CSS, Bulma, JavaScript, API Integration</p>
                                         </div>
                                         <a target='_blank' rel='noreferrer' href='https://github.com/P1Group5/trip-it.git'>
                                             <span className='bi bi-github work-icon'></span>
@@ -195,19 +195,19 @@ function Projects() {
                         <div className="card border-dark border-2 shadow-lg rounded-3">
                             <img className='shadow-lg' src={project4} alt='Project 4'>
                             </img>
-                            <div className='project-card card-img-overlay d-flex justify-content-start align-items-start px-1 py-0'>
+                            <div className='project-card card-img-overlay d-flex justify-content-start align-items-start'>
                                 {isHoveringP4 &&
                                     <Col className='hover-text text-center align-items-center'>
                                         <div>
-                                            <h1 className='fs-3 fw-bolder'>Basic Portfolio</h1>
+                                            <h1 className='project-title fs-3 fw-bolder border-bottom border-dark p-2'>Password Generator</h1>
                                         </div>
                                         <div>
-                                            <h2 className='fs-5 fw-bolder'>Project Overview</h2>
-                                            <p className='fs-6 fst-italic'>Lorem Ipsum</p>
+                                            <h2 className='sub-header fs-5 fw-bolder'>Project Overview</h2>
+                                            <p className='project-text fs-6 fst-italic'>This application makes creating a secure and custom password easy for any user. Follow the password criteria prompts and receive a strong password for greater security!</p>
                                         </div>
                                         <div>
-                                            <h2 className='fs-5 fw-bolder'>Technologies Used</h2>
-                                            <p className='fs-6 fst-italic'>Lorem Ipsum</p>
+                                            <h2 className='sub-header fs-5 fw-bolder'>Technologies Used</h2>
+                                            <p className='project-text fs-6 fst-italic'>HTML, CSS, JavaScript</p>
                                         </div>
                                         <a target='_blank' rel='noreferrer' href='https://github.com/maggiejoe/password-generator.git'>
                                             <span className='bi bi-github work-icon'></span>
@@ -225,19 +225,19 @@ function Projects() {
                         <div className="card border-dark border-2 shadow-lg rounded-3">
                             <img className='shadow-lg' src={project5} alt='Project 5'>
                             </img>
-                            <div className='project-card card-img-overlay d-flex justify-content-start align-items-start px-1 py-0'>
+                            <div className='project-card card-img-overlay d-flex justify-content-start align-items-start'>
                                 {isHoveringP5 &&
                                     <Col className='hover-text text-center align-items-center'>
                                         <div>
-                                            <h1 className='fs-3 fw-bolder'>Basic Portfolio</h1>
+                                            <h1 className='project-title fs-3 fw-bolder border-bottom border-dark p-2'>Workday Scheduler</h1>
                                         </div>
                                         <div>
-                                            <h2 className='fs-5 fw-bolder'>Project Overview</h2>
-                                            <p className='fs-6 fst-italic'>Lorem Ipsum</p>
+                                            <h2 className='sub-header fs-5 fw-bolder'>Project Overview</h2>
+                                            <p className='project-text fs-6 fst-italic'>Workday Scheduler is an application that allows the user to save events for each hour of the day. It uses several different technologies below to dynamically change the colors of each hour of the day to help keep the user organized and onto of their work schedule.</p>
                                         </div>
                                         <div>
-                                            <h2 className='fs-5 fw-bolder'>Technologies Used</h2>
-                                            <p className='fs-6 fst-italic'>Lorem Ipsum</p>
+                                            <h2 className='sub-header fs-5 fw-bolder'>Technologies Used</h2>
+                                        <p className='project-text fs-6 fst-italic'>HTML, CSS, JavaScript, jQuery, Moment.js, LocalStorage</p>
                                         </div>
                                         <a target='_blank' rel='noreferrer' href='https://github.com/maggiejoe/work-day-scheduler.git'>
                                             <span className='bi bi-github work-icon'></span>
@@ -255,19 +255,19 @@ function Projects() {
                         <div className="card border-dark border-2 shadow-lg rounded-3">
                             <img className='shadow-lg' src={project6} alt='Project 6'>
                             </img>
-                            <div className='project-card card-img-overlay d-flex justify-content-start align-items-start px-1 py-0'>
+                            <div className='project-card card-img-overlay d-flex justify-content-start align-items-start'>
                                 {isHoveringP6 &&
                                     <Col className='hover-text text-center align-items-center'>
                                         <div>
-                                            <h1 className='fs-3 fw-bolder'>Basic Portfolio</h1>
+                                            <h1 className='project-title fs-3 fw-bolder border-bottom border-dark p-2'>The Lazy Plant Caretaker</h1>
                                         </div>
                                         <div>
-                                            <h2 className='fs-5 fw-bolder'>Project Overview</h2>
-                                            <p className='fs-6 fst-italic'>Lorem Ipsum</p>
+                                            <h2 className='sub-header fs-5 fw-bolder'>Project Overview</h2>
+                                            <p className='project-text fs-6 fst-italic'>The Lazy Plant Caretaker was my second group project with the University of Toronto. This is a full stack application built with the technologies below to display everything I had learned up until that point. The main focus of this application is to help users find the perfect houseplants for their home or workspace based off of user prompts. This application takes the guess work and stress of live plants and makes it easy for you to ensure they thrive in your home!</p>
                                         </div>
                                         <div>
-                                            <h2 className='fs-5 fw-bolder'>Technologies Used</h2>
-                                            <p className='fs-6 fst-italic'>Lorem Ipsum</p>
+                                            <h2 className='sub-header fs-5 fw-bolder'>Technologies Used</h2>
+                                            <p className='project-text fs-6 fst-italic'>HTML, CSS, JavaScript, BootStrap, Node.js, Express.js, mySQL, Handlebars</p>
                                         </div>
                                         <a target='_blank' rel='noreferrer' href='https://github.com/project02group12/lazy-plant-caretaker.git'>
                                             <span className='bi bi-github work-icon'></span>
@@ -285,19 +285,19 @@ function Projects() {
                         <div className="card border-dark border-2 shadow-lg rounded-3">
                             <img className='shadow-lg' src={project7} alt='Project 7'>
                             </img>
-                            <div className='project-card card-img-overlay d-flex justify-content-start align-items-start px-1 py-0'>
+                            <div className='project-card card-img-overlay d-flex justify-content-start align-items-start'>
                                 {isHoveringP7 &&
                                     <Col className='hover-text text-center align-items-center p-0 m-0'>
                                         <div>
-                                            <h1 className='fs-3 fw-bolder'>Basic Portfolio</h1>
+                                            <h1 className='project-title fs-3 fw-bolder border-bottom border-dark p-2'>Eye Spy</h1>
                                         </div>
                                         <div>
-                                            <h2 className='fs-5 fw-bolder'>Project Overview</h2>
-                                            <p className='fs-6 fst-italic'>Lorem Ipsum</p>
+                                            <h2 className='sub-header fs-5 fw-bolder'>Project Overview</h2>
+                                            <p className='project-text fs-6 fst-italic'>Eye Spy was the last group project with the University of Toronto. It uses all the technologies below including the MERN Stack and challenged the development skills of every team member. The idea behind Eye Spy is to help users who love the outdoors find places to visit without worrying about overcrowding or over populated destinations. It uses a vote system that is user prompted to let other users know if this location is a true hidden gem or an overcrowded location.</p>
                                         </div>
                                         <div>
-                                            <h2 className='fs-5 fw-bolder'>Technologies Used</h2>
-                                            <p className='fs-6 fst-italic'>Lorem Ipsum</p>
+                                            <h2 className='sub-header fs-5 fw-bolder'>Technologies Used</h2>
+                                            <p className='project-text fs-6 fst-italic'>HTML, CSS, React, JSX, JavaScript, Bootstrap, Node.js, Express.js, GraphQL, Mongoose, MongoDB</p>
                                         </div>
                                         <div>
                                             <a className='' target='_blank' rel='noreferrer' href='https://github.com/CaptainNate/eye-spy.git'>
